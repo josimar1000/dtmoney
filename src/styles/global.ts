9560ea7dbe174eb908c6372894f1fbcf5d53462f
Josimar Lima <osimar.lima2007@gmail.com>
2021-08-29 12:01:38 -0300

    Initial application structure

---
+++ src/styles/global.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root { 
+    --red: #e52e4d;
+    --blue: #5429CC;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-body: #969CB3;
+
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  //font-size 16px (Desktop)
+  html {
+    @media (max-width: 1080px) {
+      fonte-size: 93.75%;  //15px
+    }
+
+    @media (min-width: 720px) {
+      font-size: 87.5%; //14px
+    }
+  }
+  
+  //REM = 1rem = font-size
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5,h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+
+`
