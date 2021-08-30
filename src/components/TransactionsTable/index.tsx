9018169c6c74f8b727f763adda37e3362b1b2c7e
Josimar Lima <osimar.lima2007@gmail.com>
2021-08-29 21:27:38 -0300

    create transactionsTable component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,36 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+   <Container>
+     <table>
+       <thead>
+         <tr>
+           <th>Titulo</th>
+           <th>Valor</th>
+           <th>Categoria</th>
+           <th>Data</th>
+         </tr>
+       </thead>
+
+       <tbody>
+         <tr>
+           <td>Desenvolvimento de WebSite</td>
+           <td className="deposit">R$12.0000</td>
+           <td>Desenvolvimento</td>
+           <td>20/02/2021</td>
+         </tr>
+         <tr>
+           <td>Aluguel</td>
+           <td className="withdraw">-R$1.0000</td>
+           <td>Casa</td>
+           <td>20/02/2021</td>
+         </tr>
+
+       </tbody>
+
+       
+     </table>
+   </Container>
+  )
+}
