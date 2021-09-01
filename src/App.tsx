04d4716db754bce131addef4ab17d01c5542384f
Josimar Lima <osimar.lima2007@gmail.com>
2021-08-31 23:53:21 -0300

    Using context api

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root')
 
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal 
@@ -29,6 +30,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal} 
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
